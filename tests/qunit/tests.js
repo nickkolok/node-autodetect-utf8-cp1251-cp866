6136c829cef0578ec7e609e31b97b5065179ac1c
nickkolok <nickkolok@mail.ru>
2017-04-14 01:56:52 +0300

    Corrected paths to cyrillic texts

--- tests/qunit/tests.js
+++ tests/qunit/tests.js
@@ -16,20 +16,20 @@ test('detect utf-8 in text of only latin', function () {
 test('detect cp866 in text of only cyrillic', function () {
 	assert.equal(
 		autoenc.detectEncoding(
-			fs.readFileSync( `${path}/cp866/latin866.txt` )
+			fs.readFileSync( `${path}/cp866/cyrillic866.txt` )
 		).encoding, 'cp866', 'return cp866');
 });
 
 test('detect cp1251 in text of only cyrillic', function () {
 	assert.equal(
 		autoenc.detectEncoding(
-			fs.readFileSync( `${path}/cp866/latin866.txt` )
+			fs.readFileSync( `${path}/cp1251/cyrillic1251.txt` )
 		).encoding, 'cp1251', 'return cp1251');
 });
 
 test('detect utf8 in text of only cyrillic', function () {
 	assert.equal(
 		autoenc.detectEncoding(
-			fs.readFileSync( `${path}/cp866/latin866.txt` )
+			fs.readFileSync( `${path}/utf8/cyrillicUTF8.txt` )
 		).encoding, 'utf8', 'return utf8');
 });
