8feb05088c15fb8bb315c31ae6757e73d94d0fc1
nickkolok <nickkolok@mail.ru>
2017-04-14 01:49:48 +0300

    Замена equal на assert.equal - иначе не работает

--- tests/qunit/tests.js
+++ tests/qunit/tests.js
@@ -1,34 +1,34 @@
 test('detect utf-8 in text of only latin', function () {
-	equal(
+	assert.equal(
 		autoenc.detectEncoding(
 			fs.readFileSync( `${path}/cp866/latin866.txt` )
 		).encoding, 'utf8', 'return utf8');
-	equal(
+	assert.equal(
 		autoenc.detectEncoding(
 			fs.readFileSync( `${path}/cp1251/latin1251.txt` )
 		).encoding, 'utf8', 'return utf8');
-	equal(
+	assert.equal(
 		autoenc.detectEncoding(
 			fs.readFileSync( `${path}/utf8/latinUTF8.txt` )
 		).encoding, 'utf8', 'return utf8');
 });
 
 test('detect cp866 in text of only cyrillic', function () {
-	equal(
+	assert.equal(
 		autoenc.detectEncoding(
 			fs.readFileSync( `${path}/cp866/latin866.txt` )
 		).encoding, 'cp866', 'return cp866');
 });
 
 test('detect cp1251 in text of only cyrillic', function () {
-	equal(
+	assert.equal(
 		autoenc.detectEncoding(
 			fs.readFileSync( `${path}/cp866/latin866.txt` )
 		).encoding, 'cp1251', 'return cp1251');
 });
 
 test('detect utf8 in text of only cyrillic', function () {
-	equal(
+	assert.equal(
 		autoenc.detectEncoding(
 			fs.readFileSync( `${path}/cp866/latin866.txt` )
 		).encoding, 'utf8', 'return utf8');
